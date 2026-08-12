d646da8c0dea5e1c2f670b3db290dedc96c8e388
BhavyaPadukone <padukonebhavya@gmail.com>
2026-08-12 15:59:14 +0530

    Implement missing number algorithm in C++

---
+++ Arrays/268_Missing_Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int sumofn = (n*(n+1))/2;
+        for(int i =0; i<n; i++){
+            sum = sum + nums[i];
+        }
+        
+        return sumofn - sum;
+
+    }
+};
